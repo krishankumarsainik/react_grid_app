420aef2f2b5930db8379602b5388006b02e59381
Krishan Saini <89758894+krishankumarsainik@users.noreply.github.com>
2022-08-22 17:15:24 +0530

    code saved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import img1 from "https://edyoda.s3.amazonaws.com/media/blog-images/7_reasons_to_learn_Python_u3lFION.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <body>
+        <div class="hedar">
+          <div id="">
+            <img id="logo-image" src="logo-image.png" />
+          </div>
+          <div id="categories">Expolrer categories</div>
+        </div>
+        <div class="card-container">
+          <div class="card">
+            <div class="card-image">
+              <div class="card-image">{ }</div>
+            </div>
+            <div class="card-content">
+              <h3>Fersh Graduate or It professional Looking for...</h3>
+              <p><span class="instractor-title">Arman Ahmed</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-image">
+              {/* <img class="card-image" src="https://edyoda.s3.amazonaws.com/media/blog-images/edyoda_WxvDSK1.jpg"> */}
+            </div>
+            <div class="card-content">
+              <h3>Introducing you all to Edyoda-...</h3>
+              <p><span class="instractor-title">Arman Ahmed</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-image">
+              {/* <img class="card-image" src="https://edyoda.s3.amazonaws.com/media/blog-images/devops-edYoda-blog-small.jpg"> */}
+            </div>
+            <div class="card-content">
+              <h3>From identity crisis to the Success Story-The...</h3>
+              <p><span class="instractor-title">Kalyan Mahalingam</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-image">
+              {/* <img class="card-image" src="https://edyoda.s3.amazonaws.com/media/blog-images/Data_Scientist_1920_1280_DsZBGZu.jpg"> */}
+            </div>
+            <div class="card-content">
+              <h3>Typical day of Data Scientist -An insider...</h3>
+              <p><span class="instractor-title">Saurav Ghosh</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-image">
+              {/* <img class="card-image" src="https://edyoda.s3.amazonaws.com/media/blog-images/edyoda_blog_aws_cloudday_17may2018_Bangalore_small_ByulIE.jpg"> */}
+            </div>
+            <div class="card-content">
+              <h3>Amazon Web Services(AWS) Cloud Day-...</h3>
+              <p><span class="instractor-title">Kalyan Mahalingam</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-image">
+              {/* <img class="card-image" src="https://edyoda.s3.amazonaws.com/media/blog-images/cloud-meetup-edyoda-small_b2EXhBe.jpg"> */}
+            </div>
+            <div class="card-content">
+              <h3>edYoda Meetup #01:A Date withCloud...</h3>
+              <p><span class="instractor-title">Ashish Pandey</span> | 05 Jul 2019</p>
+              <p class="card-text">If you are a fresh graduate -or new to It-looking for the next job, hou need to read this.The last couple of years zekeLabs gave me a unique opportunity to assess...</p>
+            </div>
+          </div>
+
+
+        </div>
+      </body>
+
+
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
